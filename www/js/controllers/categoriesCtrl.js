d09a68777c8cd0d716469a48a1f4f21b5955e866
ji <gafrong@gmail.com>
2016-08-04 09:52:59 +1000

    modal medai query fix

--- www/js/controllers/categoriesCtrl.js
+++ www/js/controllers/categoriesCtrl.js
@@ -1,6 +1,6 @@
 AppCtrl
 .controller('CategoriesCtrl', function($scope) {
-	$scope.setActive=function(category){
+  $scope.setActive=function(category){
     $scope.activeCategory = category;
   };
 
@@ -8,13 +8,13 @@ AppCtrl
     {
       id: 0, title: 'travel', imageUrl:"http://media3.s-nbcnews.com/j/MSNBC/Components/Photo/_new/110421-nat-forest2-hmed.grid-10x2.jpg"
     },{
-      id: 1, title: 'health', imageUrl:"https://pixabay.com/static/uploads/photo/2015/03/26/10/05/avocado-690898_960_720.jpg"
+      id: 1, title: 'health', imageUrl:"https://authoritynutrition.com/wp-content/uploads/2014/09/avocado-sliced-in-half.jpg"
     },{
       id: 2, title: 'music', imageUrl:"http://i0.wp.com/www.themusiczoo.com/blog/wp-content/uploads/2015/03/carousel-image1.jpg?resize=500%2C250"
     },{
       id: 3, title: 'sports', imageUrl:"https://s-media-cache-ak0.pinimg.com/originals/21/99/fb/2199fbc6b13e7ed41d1e804217b79718.jpg"
     },{
-      id: 4, title: 'space', imageUrl:"https://pixabay.com/static/uploads/photo/2015/10/12/14/59/milky-way-984050_960_720.jpg"
+      id: 4, title: 'space', imageUrl:"http://cache1.asset-cache.net/xd/503936820.jpg?v=1&c=IWSAsset&k=2&d=DF8D445051B40C749E1C9693F348F87C4E7679DD01108BC6D7AA5656A2D92D71B74067DC4F41F14B"
     }
   ]
 })
